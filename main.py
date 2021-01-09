5db1d0aff1a4e0f3324aea2368f923f07b6c6d5c
safaak4 <cumasafak@gmail.com>
2021-01-10 02:31:47 +0300

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtWidgets import QLineEdit, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QMainWindow, QSizePolicy, QWidget, \
-    QGridLayout
+    QGridLayout, QCheckBox
 from PyQt5.QtCore import QSize, QPoint, Qt, QUrl
 from  PyQt5.QtGui import *
 
@@ -14,13 +14,15 @@ class KiwiWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
-        #self.setFocusPolicy(Qt.StrongFocus)
 
         self.show()
 
     def init_ui(self):
         # Title bar off
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+        self.myFont = QtGui.QFont("Uni Sans", 10)
+        self.myFont.setItalic(True)
 
         #Main Layout
         self.mainlayout = QGridLayout()
@@ -32,11 +34,61 @@ class KiwiWindow(QtWidgets.QWidget):
         self.mainlayout.addLayout(self.leadlayout, 0, 0, 0, 0)
         self.leadlayout.layout().setContentsMargins(0, 130, 0, 0)
 
-        #WebView
-        self.webWebWiew = QWebEngineView()
-        self.leadlayout.addWidget(self.webWebWiew, 0, 0, 0, 1)
-        self.webWebWiew.close()
-        self.webWebWiew.setFocusPolicy(Qt.StrongFocus)
+        #Login Layout
+        self.loginlayout = QVBoxLayout()
+        self.leadlayout.addLayout(self.loginlayout,0,0,0,0, Qt.AlignTop)
+
+
+        self.usernameText = QLabel("Username")
+        self.loginlayout.addWidget(self.usernameText)
+        self.usernameText.setStyleSheet("""color: #34495e; """)
+        self.usernameText.setFont(QFont(self.myFont))
+        self.loginlayout.setSpacing(0)
+
+        self.usernameline = QLineEdit()
+        self.loginlayout.addWidget(self.usernameline)
+        #self.usernameline.setContentsMargins(0,100,0,0)
+        self.usernameline.setFixedSize(300,35)
+        self.usernameline.setStyleSheet("""
+        background-color: #16a085;
+        color: white;
+        border: none;
+        font: 19px;
+        border-radius: 10px;
+        """)
+        self.loginlayout.setSpacing(0)
+
+        self.passwordText = QLabel("Password")
+        self.loginlayout.addWidget(self.passwordText)
+        self.passwordText.setStyleSheet("""color: #34495e; """)
+        self.passwordText.setFont(QFont(self.myFont))
+        self.loginlayout.setSpacing(0)
+
+        self.passwordline = QLineEdit()
+        self.loginlayout.addWidget(self.passwordline)
+        # self.usernameline.setContentsMargins(0,100,0,0)
+        self.passwordline.setFixedSize(300, 35)
+        self.passwordline.setStyleSheet("""
+                background-color: #16a085;
+                color: white;
+                border: none;
+                font: 19px;
+                border-radius: 10px;
+                """)
+        self.passwordline.setEchoMode(QLineEdit.Password)
+        self.loginlayout.setSpacing(0)
+
+        self.chckbox = QCheckBox("Remember Me")
+        self.loginlayout.addWidget(self.chckbox)
+        #self.chckbox.setStyleSheet("""QCheckBox::indicator {
+        #border: 3px solid #5A5A5A;
+        #background: none;
+        #}""")
+
+        self.loginButton = QPushButton("Login")
+        self.loginButton.setFixedSize(300,35)
+        self.loginlayout.addWidget(self.loginButton)
+
 
         #Title Layout
         self.titlelayout = QGridLayout()
@@ -44,106 +96,96 @@ class KiwiWindow(QtWidgets.QWidget):
         self.mainlayout.addLayout(self.titlelayout, 0,0,0,0)
         self.titlelayout.setContentsMargins(0,0,0,0)
         self.titlelayout.setAlignment(Qt.AlignTop | Qt.AlignLeft)
-        self.titlelayout.sizeHint().setHeight(35)
+        #self.titlelayout.sizeHint().setHeight(35)
 
         #Functions Layout (in Title Layout)
         self.functionslayout = QHBoxLayout()
-        self.titlelayout.addLayout(self.functionslayout,1,0,0,0)
-        self.functionslayout.setAlignment(Qt.AlignRight)
+        self.titlelayout.addLayout(self.functionslayout,0,0,0,2)
+        self.functionslayout.setAlignment(Qt.AlignRight | Qt.AlignTop)
         self.functionslayout.setSpacing(0)
 
         #Top Navigation Layout
         self.topnavigation = QHBoxLayout()
-        self.mainlayout.addLayout(self.topnavigation, 0,0,0,0, Qt.AlignTop)
-        self.topnavigation.layout().setContentsMargins(0,40,0,0)
+        self.titlelayout.addLayout(self.topnavigation, 0,0,0,0, Qt.AlignLeft)
+        #self.topnavigation.layout().setContentsMargins(0,0,0,0)
 
-        #Top Navigation Bar
-        self.buttonInstagram = QPushButton("Instagram")
-        self.buttonFacebook = QPushButton("Facebook")
-        self.buttonTwitter = QPushButton("Twitter")
-
-        sizePolicy3 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        sizePolicy3.setHeightForWidth(self.buttonInstagram.sizePolicy().hasHeightForWidth())
-
-        self.buttonFont = QtGui.QFont("Uni Sans", 13)
-        self.buttonFont.setItalic(True)
-        self.buttonInstagram.setSizePolicy(sizePolicy3)
-        self.buttonInstagram.setFixedSize(QSize(150, 90))
-        self.buttonInstagram.setStyleSheet("QPushButton::!hover"
-        "{"
-        "color: #cccccc;"
-        "}"
-        "QPushButton::hover"
-        "{"
-        "color: white;"
-        "border-bottom-width: 1px;"
-        "border-radius: 0px;"
-        "text-decoration: underline;"
-        "}")
-        self.buttonInstagram.setFont(self.buttonFont)
-        self.buttonInstagram.setFlat(True)
-        self.buttonInstagram.clicked.connect(self.btn_instagram_clicked)
-
-        self.buttonFacebook.setSizePolicy(sizePolicy3)
-        self.buttonFacebook.setFixedSize(QSize(150, 90))
-        self.buttonFacebook.setStyleSheet("QPushButton::!hover"
-        "{"
-        "color: #cccccc;"
-        "}"
-        "QPushButton::hover"
-        "{"
-        "color: white;"
-        "border-bottom-width: 1px;"
-        "border-radius: 0px;"
-        "text-decoration: underline;"
-        "}")
-        self.buttonFacebook.setFont(self.buttonFont)
-        self.buttonFacebook.setFlat(True)
-        self.buttonFacebook.clicked.connect(self.btn_facebook_clicked)
-
-        self.buttonTwitter.setSizePolicy(sizePolicy3)
-        self.buttonTwitter.setFixedSize(QSize(150, 90))
-        self.buttonTwitter.setStyleSheet(
-        "QPushButton::!hover"
-        "{"
-        "color: #cccccc;"
-        "}"
-        "QPushButton::hover"
-        "{"
-        "color: white;"
-        "border-bottom-width: 1px;"
-        "border-radius: 0px;"
-        "text-decoration: underline;"
-        "}"
-        )
-        self.buttonTwitter.setFont(self.buttonFont)
-        self.buttonTwitter.setFlat(True)
-        self.buttonTwitter.clicked.connect(self.btn_twitter_clicked)
-
-        self.topnavigation.addWidget(self.buttonInstagram, 0)
-        self.topnavigation.addWidget(self.buttonFacebook, 1)
-        self.topnavigation.addWidget(self.buttonTwitter, 2)
-
-        #Title Bar
-        self.myFont = QtGui.QFont("Uni Sans", 12)
-        self.myFont.setItalic(True)
-        self.title = QLabel("Kiwii App nnn")
+        # Title Bar
+        self.title = QLabel("")
         self.title.setFont(self.myFont)
-        self.title.setFixedHeight(35)
+        self.title.setFixedHeight(90)
         self.title.setAlignment(Qt.AlignLeft)
-        self.titlelayout.addWidget(self.title, 3, 0, 0, 0)
+        self.titlelayout.addWidget(self.title, 0, 0, 0, 0)
         self.title.setStyleSheet("""
-                            background-color: #150485;
-                            color: white;
-                            padding-top: 5px;
-                            padding-left: 5px;
-                        """)
+                                   background-color: #16a085;
+                                   color: white;
+                                   padding-top: 5px;
+                                   padding-left: 5px;
+                               """)
         self.title.setFixedWidth(self.width())
         # self.title.resize(self.width(), 35)
         # self.title.setSizePolicy(QSizePolicy.Expanding, 35)
         self.title.setMouseTracking(True)
         self.title.mousePressEvent = self.mouseTitlePressEvent
         self.title.mouseMoveEvent = self.mouseTitleMoveEvent
+
+        #Top Navigation Bar
+
+        self.buttonsettings = """QPushButton::!hover
+                                          {
+                                          color: #34495e;
+                                          }
+                                          QPushButton::hover
+                                          {
+                                          color: white;
+                                          border-bottom-width: 1px;
+                                          border-radius: 0px;
+                                          text-decoration: underline;
+                                          }"""
+
+        self.buttonInstagram = QPushButton("Instagram")
+        self.buttonFacebook = QPushButton("Facebook")
+        self.buttonTwitter = QPushButton("Twitter")
+        self.buttonMainPage = QPushButton("Kiwii App")
+
+        sizePolicy3 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy3.setHeightForWidth(self.buttonInstagram.sizePolicy().hasHeightForWidth())
+
+        self.buttonFont = QtGui.QFont("Uni Sans", 14)
+        self.buttonFont.setItalic(True)
+        self.buttonFont.setBold(True)
+
+        self.buttonMainPage.setSizePolicy(sizePolicy3)
+        self.buttonMainPage.setFixedSize(QSize(150, 90))
+        self.buttonMainPage.setStyleSheet(self.buttonsettings)
+        self.buttonMainPage.setFont(self.buttonFont)
+        self.buttonMainPage.setFlat(True)
+        #self.buttonFacebook.clicked.connect(self.btn_facebook_clicked)
+
+        self.buttonInstagram.setSizePolicy(sizePolicy3)
+        self.buttonInstagram.setFixedSize(QSize(150, 90))
+        self.buttonInstagram.setStyleSheet(self.buttonsettings)
+        self.buttonInstagram.setFont(self.buttonFont)
+        self.buttonInstagram.setFlat(True)
+        self.buttonInstagram.clicked.connect(self.btn_instagram_clicked)
+
+        self.buttonFacebook.setSizePolicy(sizePolicy3)
+        self.buttonFacebook.setFixedSize(QSize(150, 90))
+        self.buttonFacebook.setStyleSheet(self.buttonsettings)
+        self.buttonFacebook.setFont(self.buttonFont)
+        self.buttonFacebook.setFlat(True)
+        self.buttonFacebook.clicked.connect(self.btn_facebook_clicked)
+
+        self.buttonTwitter.setSizePolicy(sizePolicy3)
+        self.buttonTwitter.setFixedSize(QSize(150, 90))
+        self.buttonTwitter.setStyleSheet(self.buttonsettings)
+        self.buttonTwitter.setFont(self.buttonFont)
+        self.buttonTwitter.setFlat(True)
+        self.buttonTwitter.clicked.connect(self.btn_twitter_clicked)
+
+        self.topnavigation.addWidget(self.buttonMainPage, 0)
+        self.topnavigation.addWidget(self.buttonInstagram, 1)
+        self.topnavigation.addWidget(self.buttonFacebook, 2)
+        self.topnavigation.addWidget(self.buttonTwitter, 3)
 
         #Path File
         scriptDir = os.path.dirname(os.path.realpath(__file__))
@@ -158,7 +200,7 @@ class KiwiWindow(QtWidgets.QWidget):
         self.btn_minimize.setIconSize(QSize(14,14))
         self.btn_minimize.setStyleSheet("QPushButton::hover"
         "{"
-        "background-color: #0d0063;"
+        "background-color: #72c4db;"
         "border-bottom-width: 1px;"
         "border-radius: 0px;"
         "text-decoration: underline;"
@@ -172,7 +214,7 @@ class KiwiWindow(QtWidgets.QWidget):
         self.btn_maximize.setStyleSheet(
         "QPushButton::hover"
         "{"
-        "background-color: #0d0063;"
+        "background-color: #72c4db;"
         "border-bottom-width: 1px;"
         "border-radius: 0px;"
         "text-decoration: underline;"
@@ -192,7 +234,7 @@ class KiwiWindow(QtWidgets.QWidget):
         self.btn_close.setIconSize(QSize(14, 14))
         self.btn_close.setStyleSheet("QPushButton::hover"
         "{"
-        "background-color: #0d0063;"
+        "background-color: #72c4db;"
         "border-bottom-width: 1px;"
         "border-radius: 0px;"
         "text-decoration: underline;"
@@ -203,35 +245,14 @@ class KiwiWindow(QtWidgets.QWidget):
         sizegrip = QtWidgets.QSizeGrip(window)
         self.mainlayout.addWidget(sizegrip, 2, 0, 1, 0, Qt.AlignRight | Qt.AlignBottom)
 
-    def keyPressEvent(self, event):
-
-        if self.webWebWiew.url() == QUrl("https://www.instagram.com/"):
-            self.webWebWiew.page().runJavaScript(
-                "document.querySelector('#loginForm > div > div:nth-child(1) > div > label > input').value;", self.denemee)
-        else:
-            print("nolu")
-
-
     def webPageChange(self, pagex):
         if pagex == 0:
-            self.webWebWiew.close()
-            self.webWebWiew.load(QUrl("https://www.instagram.com/"))
-            self.webWebWiew.show()
-
-           # self.webWebWiew.page().runJavaScript(
-            #         "document.querySelector('#loginForm > div > div:nth-child(1) > div > label > input').value;", self.denemee)
-
+            print("Instagram")
 
         elif pagex == 1:
-            self.webWebWiew.close()
-            self.webWebWiew.load(QUrl("https://www.facebook.com/"))
-            self.webWebWiew.show()
-
+            print("Facebook")
         else:
-            self.webWebWiew.close()
-            self.webWebWiew.load(QUrl("https://twitter.com/login"))
-            self.webWebWiew.show()
-
+            print("Twitter")
 
     def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
         self.title.setFixedWidth(self.width())
@@ -258,28 +279,8 @@ class KiwiWindow(QtWidgets.QWidget):
                                            "text-decoration: underline;"
                                            "}"
                                            )
-        self.buttonFacebook.setStyleSheet("QPushButton::!hover"
-                                          "{"
-                                          "color: #cccccc;"
-                                          "}"
-                                          "QPushButton::hover"
-                                          "{"
-                                          "color: white;"
-                                          "border-bottom-width: 1px;"
-                                          "border-radius: 0px;"
-                                          "text-decoration: underline;"
-                                          "}")
-        self.buttonTwitter.setStyleSheet("QPushButton::!hover"
-                                          "{"
-                                          "color: #cccccc;"
-                                          "}"
-                                          "QPushButton::hover"
-                                          "{"
-                                          "color: white;"
-                                          "border-bottom-width: 1px;"
-                                          "border-radius: 0px;"
-                                          "text-decoration: underline;"
-                                          "}")
+        self.buttonFacebook.setStyleSheet(self.buttonsettings)
+        self.buttonTwitter.setStyleSheet(self.buttonsettings)
         self.webPageChange(0)
 
     def btn_facebook_clicked(self):
@@ -294,28 +295,8 @@ class KiwiWindow(QtWidgets.QWidget):
                                            "border-radius: 0px;"
                                            "text-decoration: underline;"
                                            "}")
-        self.buttonTwitter.setStyleSheet("QPushButton::!hover"
-                                          "{"
-                                          "color: #cccccc;"
-                                          "}"
-                                          "QPushButton::hover"
-                                          "{"
-                                          "color: white;"
-                                          "border-bottom-width: 1px;"
-                                          "border-radius: 0px;"
-                                          "text-decoration: underline;"
-                                          "}")
-        self.buttonInstagram.setStyleSheet("QPushButton::!hover"
-                                           "{"
-                                           "color: #cccccc;"
-                                           "}"
-                                           "QPushButton::hover"
-                                           "{"
-                                           "color: white;"
-                                           "border-bottom-width: 1px;"
-                                           "border-radius: 0px;"
-                                           "text-decoration: underline;"
-                                           "}")
+        self.buttonTwitter.setStyleSheet(self.buttonsettings)
+        self.buttonInstagram.setStyleSheet(self.buttonsettings)
         self.webPageChange(1)
 
     def btn_twitter_clicked(self):
@@ -330,32 +311,9 @@ class KiwiWindow(QtWidgets.QWidget):
                                            "border-radius: 0px;"
                                            "text-decoration: underline;"
                                            "}")
-        self.buttonFacebook.setStyleSheet("QPushButton::!hover"
-                                          "{"
-                                          "color: #cccccc;"
-                                          "}"
-                                          "QPushButton::hover"
-                                          "{"
-                                          "color: white;"
-                                          "border-bottom-width: 1px;"
-                                          "border-radius: 0px;"
-                                          "text-decoration: underline;"
-                                          "}")
-        self.buttonInstagram.setStyleSheet("QPushButton::!hover"
-                                          "{"
-                                          "color: #cccccc;"
-                                          "}"
-                                          "QPushButton::hover"
-                                          "{"
-                                          "color: white;"
-                                          "border-bottom-width: 1px;"
-                                          "border-radius: 0px;"
-                                          "text-decoration: underline;"
-                                          "}")
+        self.buttonFacebook.setStyleSheet(self.buttonsettings)
+        self.buttonInstagram.setStyleSheet(self.buttonsettings)
         self.webPageChange(2)
-
-    def denemee(self, value):
-        print(str(value))
 
     def btn_close_clicked(self):
         self.close()
@@ -374,7 +332,7 @@ class KiwiWindow(QtWidgets.QWidget):
 app = QtWidgets.QApplication(sys.argv)
 pencere = KiwiWindow()
 pencere.setWindowTitle("Kiwi App")
-pencere.setStyleSheet("background-color: #080136")
+pencere.setStyleSheet("background-color: #1abc9c")
 #pencere.setMaximumSize(1280,720)
 pencere.setMinimumSize(1280,720)
 #pencere.setFixedSize(1280, 720)
